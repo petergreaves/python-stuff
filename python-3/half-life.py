2177fb34b0269b23ffee118030e86b356d375a75
peter greaves <peter.greaves64@gmail.com>
2025-03-21 07:49:39 +0000

    added plotAll

--- python-3/half-life.py
+++ python-3/half-life.py
@@ -4,52 +4,77 @@
 # 2. Prompts the user for an initial amount of the isotope (in grams) and an elapsed time (in seconds).
 # 3. Calculates the amount of the isotope remaining after this elapsed time and displays the result.
 # 4. Calculates the initial mass of the isotope (in grams) necessary to ensure that, after half a minute, 5 grams of it remain, and displays the result. 5
-# Plots the amount of the isotope remaining as a function of time, from elapsed time
-# 6. The program should then
-# repeat the instructions from step 1, until the user chooses to halt the program.
+# 5. Plots the amount of the isotope remaining as a function of time, from elapsed time
+# 6. The program should then repeat the instructions from step 1, until the user chooses to halt the program.
 # 7. When the user wishes to enter no more isotopes, the program should display the decay plots for all of the isotopes that were entered, in a single
 # plot, and print out a list of all the isotopes studied.
-
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+# constants for plotting on the x axis
+plotStart = 0
+plotEnd = 10
+plotGranularity = 110
 
-def sigfigs(v, sfs):
-    return np.format_float_positional(v, precision=sfs, unique=False, fractional=False, trim='k')
+
+# function to return a float to the required SFs
+def sigfigs(v, sigfigs):
+    return np.format_float_positional(v, precision=sigfigs, unique=False, fractional=False, trim='k')
 
 
-def getRemainingAtElapsedTime(isotopeDetails, t):
-    print("t=",isotopeDetails["elapsedTime"])
-    print("amount",isotopeDetails["amount"])
-    print("hl",isotopeDetails["halfLife"])
+# function to returns the remaining mass (g) for an isotope at time t (s)
+def getRemainingMassAtElapsedTime(isotopeDetails, t):
+    print("t=", isotopeDetails["elapsedTime"])
+    print("amount", isotopeDetails["amount"])
+    print("hl", isotopeDetails["halfLife"])
     remaining = isotopeDetails["amount"] * (0.5 ** (t / isotopeDetails["halfLife"]))
-    print("remaining=",remaining)
+    # print("remaining=", remaining)
     return isotopeDetails["amount"] * (0.5 ** (t / isotopeDetails["halfLife"]))
 
 
+# function to return the initial mass (g) for an isotope if the specified gramsRequired (g)
+# were to remain at elapsedTime (s)
 def getInitialMass(isotopeDetails, elapsedTime, gramsRequired):
-    initialMass = gramsRequired / (0.5 ** (elapsedTime / isotopeDetails["halfLife"]))
-    return initialMass
+    return gramsRequired / (0.5 ** (elapsedTime / isotopeDetails["halfLife"]))
 
 
-def plot(isotopeDetails, t):
-    t = np.linspace(0, 10, 11)
-    print("t=",t)
-    y = getRemainingAtElapsedTime(isotopeDetails, t)
-    print("y=",y)
-    #plt.title("Half life of isotope", isotopeDetails["isotopeName"])
+# function to plot isotope decay for all the isotopes in the list
+def plotAll(isotopes):
+    t = np.linspace(plotStart, plotEnd, plotGranularity)
+    ys=[]
+    for i in isotopes:
+        ys.append(getRemainingMassAtElapsedTime(i, t))
+        print(ys)
+    plt.title("Decay rate of isotopes")
+    plt.xlabel("Time/s")
+    plt.ylabel("Amount remaining/g")
+    for y in ys:
+        plt.plot(t, y)
+    plt.show()
+
+
+
+# function to plot isotope decay for all the isotopes in the list
+def plot(isotope):
+    t = np.linspace(plotStart, plotEnd, plotGranularity)
+    y = getRemainingMassAtElapsedTime(isotope, t)
+    plt.title("Decay rate of isotope " + isotope["isotopeName"])
+    plt.xlabel("Time/s")
+    plt.ylabel("Amount remaining/g")
     plt.plot(t, y)
     plt.show()
 
 
-# grab some user input into a dict and return it
+# function to get user input for the isotope into a dictionary and return it.
+# if the user enters q for the isotope name, it just returns an empty dictionary
+# so the caller can work out what to do
 def getIsotopeDetails():
     isotopeName = input("Enter the isotope name, or q to quit:")
     if isotopeName == 'q':
         return {}
     else:
-        halflife = int(input("Enter the half life (seconds):"))
+        halflife = float(input("Enter the half life (seconds):"))
         amount = int(input("Enter the starting amount (grams):"))
         elapsedTime = int(input("Enter the elapsed time (seconds):"))
     return {"isotopeName": isotopeName,
@@ -58,19 +83,45 @@ def getIsotopeDetails():
             "elapsedTime": elapsedTime}
 
 
-thisIsotopeDetails = getIsotopeDetails()
+# function to do the end-of-program summary
+def summarise():
+    #for isotope in allIsotopeDetails:
+    #   plot(isotope)
+    plotAll(allIsotopeDetails)
+    # print all the isotope names (if any)
+    if len(allIsotopeDetails) > 0:
+        print("Thanks for using this app! The isotopes we studied were:")
+        for isotope in allIsotopeDetails:
+            print(isotope["isotopeName"])
 
-if thisIsotopeDetails == {}:
-    print("Bye!")
-else:
-    # Calculate the amount of the isotope remaining after this elapsed time and display the result.
-# print("1.", str(getRemainingAtElapsedTime(thisIsotopeDetails, thisIsotopeDetails["elapsedTime"])), "grams of",
-    #      thisIsotopeDetails["isotopeName"], "remain after", str(thisIsotopeDetails["elapsedTime"]), "seconds")
 
-    # Calculate the initial mass of the isotope (in grams) necessary to ensure that, after half a minute,
-    #  5 grams of it remain, and display the result.
-  #  print("2. When 30s have passed, if 5g remain, then the initial mass was",
-    #      sigfigs(getInitialMass(thisIsotopeDetails, 30, 5), 2), "g to 2SF")
+# list for where we keep all the isotopes that were studied so
+# we can iterate it at the end
+allIsotopeDetails = []
 
-    # Plot the amount of the isotope remaining as a function of time, from elapsed time t=0 to t=10
-    plot(thisIsotopeDetails, 10)
+# THE MAIN PROGRAM/ENTRY POINT
+while True:
+    # get the isotope details
+    thisIsotopeDetails = getIsotopeDetails()
+    # empty isotope means time to quit, so
+    if thisIsotopeDetails == {}:
+        summarise()
+        break
+    else:
+        # Calculate the amount of the isotope remaining after this elapsed time and display the result.
+        print("1.", str(sigfigs(getRemainingMassAtElapsedTime(thisIsotopeDetails, thisIsotopeDetails["elapsedTime"]),2)),
+              "grams (to 2SF) of",
+              thisIsotopeDetails["isotopeName"], "remain after", str(thisIsotopeDetails["elapsedTime"]), "seconds")
+
+        # Calculate the initial mass of the isotope (in grams) necessary to ensure that, after half a minute,
+        # 5 grams of it remain, and display the result.
+        print("2. When 30s have passed, if 5g remain, then the initial mass was",
+              sigfigs(getInitialMass(thisIsotopeDetails, 30, 5), 2), "g to 2SF")
+
+        # Plot the amount of the isotope remaining as a function of time,
+        # from elapsed time t=0 to t=10
+        plot(thisIsotopeDetails)
+
+        # save this isotope to the list
+        allIsotopeDetails.append(thisIsotopeDetails)
+
