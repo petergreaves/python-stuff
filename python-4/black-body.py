bf131e4d8f381a4f115a166303434c47ae7dffac
peter greaves <peter.greaves64@gmail.com>
2025-04-27 13:52:15 +0100

    black body program v1

--- python-4/black-body.py
+++ python-4/black-body.py
@@ -8,7 +8,9 @@ c = 2.998e8
 k = 1.381e-23
 
 
-# calculate a BB curve for the wavelenths and temperature provided
+# calculate a BB curve for the wavelenths (m) and temperature (K) provided
+# verbose just writes out intermediate steps
+# in the calculation
 def bbCurve(wavelengths, temperature, verbose):
     # for each wavelength and temp combination, calculate
     # the result
@@ -19,19 +21,18 @@ def bbCurve(wavelengths, temperature, verbose):
         partOneNumerator = (2 * planck_h * (c ** 2))
         partOneDenominator = (w ** 5)
         if verbose:
-            print("partOneNumerator/partOneDenominator", partOneNumerator/partOneDenominator)
+            print("partOneNumerator/partOneDenominator", partOneNumerator / partOneDenominator)
         expVal = planck_h * c / (w * k * temperature)
         if verbose:
             print("the exponent", expVal)
-        partTwo=1 / (np.exp(expVal) - 1)
+        partTwo = 1 / (np.exp(expVal) - 1)
         if verbose:
             print("1/ e^the exponent -1)", partTwo)
-        result =  partOneNumerator / partOneDenominator \
+        result = partOneNumerator / partOneDenominator \
                  * partTwo
         if verbose:
             print("for w=", w, ", the result is", result)
         intensities.append(result)
-
 
     plt.xlabel('wavelength / mm')
     plt.ylabel('intensity / ?')
@@ -39,13 +40,18 @@ def bbCurve(wavelengths, temperature, verbose):
     plt.show()
     return
 
-# Define the wavelength range in millimetres
-lambda_range=np.linspace(0.1,5.0,50,endpoint=True)
-# convert to metres
 
-temperature = 2
-verbose=False
-lambda_range_metres=lambda_range*1e-3
-bbCurve(lambda_range_metres, temperature,verbose)
-#for k in [1,2,3,4,5,6,7,8,9,10]:
-    #bbCurve(wavelengths, k,verbose)
+
+# Ask for the temperature for which the intensity of
+# radiation will be plotted
+temperature=float(input("Enter a temp between 1 and 10 K "))
+
+# Define the wavelength range in millimetres
+lambda_range = np.linspace(0.1, 5.0, 50, endpoint=True)
+# convert to metres because this is what our function expects
+lambda_range_metres = lambda_range * 1e-3
+
+verbose = False
+bbCurve(lambda_range_metres, temperature, verbose)
+
+
---
+++ python-4/temp-guesser.py
@@ -0,0 +1,127 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# some constants
+planck_h = 6.626e-34
+c = 2.998e8
+#boltzman
+k = 1.381e-23
+
+# Data from the COBE satellite
+# The data is provided in two arrays below.
+
+# Measured data: wavelength is in millimetres
+measured_lambdas = np.array([4.405286344, 3.676470588, 3.144654088, \
+                             2.754820937, 2.450980392, 2.004008016, 1.834862385, 1.377410468, \
+                             0.881834215, 0.468823254], float)
+# Measured data: intensity in W m**-2 m**-1 sr**-1
+# these are the intensites we compare our calculated values to, using root mean square
+measured_intensities = np.array([3.10085E-05, 5.53419E-05, 8.8836E-05, \
+                                 0.000129483, 0.000176707, 0.000284786, 0.00034148, 0.000531378, \
+                                 0.000561909, 6.16936E-05], float)
+
+# where we put the temperature and calculated intensities for that temp
+resultingTempIntensitiesAndRMSDs = []
+
+
+# calculate intensities lambdas (in mm) given a temperature
+# (a float) using the  ...  verbose=True will report its working in a detailed way
+# returned value are the calculated intensities
+def intensitiesForTemperature(lambdas_in_mm, temperature, verbose):
+    # for each lambda and temp combination, calculate
+    # the result
+    if verbose:
+        print("With temperature", temperature, "wl=", " and measured lambda=", lambdas_in_mm)
+    calculated_intensities = []
+
+    # convert to metres
+    lambdas_in_m = lambdas_in_mm * 1e-3
+
+    # let's calculate the intensities from the temperature and measured lambdas
+    for l in lambdas_in_m:
+        partOneNumerator = (2 * planck_h * (c ** 2))
+        partOneDenominator = (l ** 5)
+        if verbose:
+            print("partOneNumerator/partOneDenominator", partOneNumerator / partOneDenominator)
+        expVal = planck_h * c / (l * k * temperature)
+        if verbose:
+            print("the exponent", expVal)
+        partTwo = 1 / (np.exp(expVal) - 1)
+        if verbose:
+            print("1/ e^the exponent -1)", partTwo)
+        res = partOneNumerator / partOneDenominator \
+              * partTwo
+        if verbose:
+            print("for w=", l, ", the result is", res)
+        calculated_intensities.append(res)
+    return calculated_intensities
+
+
+# given two arrays,1 what is the root mean square deviation?
+# return this as a float
+def rootMeanSquareDev(measured, calculated):
+    numberOfValues = len(measured)
+    squareOfTheDeviations = (measured - calculated) ** 2
+    rmsd = np.sqrt(sum(squareOfTheDeviations) / numberOfValues)
+    return rmsd
+
+
+def plotResult(x, y, title):
+    plt.title(title)
+    plt.xlabel("wavelength / mm")
+    plt.ylabel("intensity / W m^−2 sf^-1 m^-1")
+    plt.plot(x, y)
+    plt.show()
+    return
+
+
+# storage for the result with the lowest root mean square deviation that
+# we have found by repeated guesses
+currentLowestRMSD = []
+
+# main loop
+while True:
+    temperatureAsText = input("Enter a temperature between 1 and 10 K, or q to quit > ")
+    # we decided to exit
+    if temperatureAsText == 'q':
+        # there is something to plot, so plot the result
+        if len(currentLowestRMSD) > 0:
+            plotResult(measured_lambdas * 1e-3, \
+                       currentLowestRMSD[0]["intensities"],
+                       "Intensities by wavelength for temperature " + \
+                       str(currentLowestRMSD[0]["temp"]) + " K")
+        else:
+            print("Bye!")
+            exit(0)
+
+    # optimistically convert the temperature to a float
+    temperature = float(temperatureAsText)
+
+    # calculate the intensities for this temperature
+    calculatedIntensities = intensitiesForTemperature(measured_lambdas, temperature, False)
+
+    # calculate the root mean square deviation for these intensities, compared with
+    # measured intensities
+    thisRMSD = rootMeanSquareDev(measured_intensities, calculatedIntensities)
+
+    # test the result to see if it is better (lower) that the current guess
+    # the first time, we just store it because it is automatically the minimum so far
+    if len(currentLowestRMSD) == 0:  # first one, so this will be the best guess so far
+        print("Saving the first result with a root mean square deviation of", thisRMSD)
+        currentLowestRMSD.append({"temp": temperature, "rmsd": thisRMSD, "intensities": calculatedIntensities})
+    # compare the new root mean square deviation to one we have already stored
+    # if it is lowest, replace it ...
+    elif thisRMSD < currentLowestRMSD[0]["rmsd"]:
+        print("Your latest guess has a lower root mean square deviation (" \
+              , thisRMSD, \
+              ") than currently stored, so we are saving it ... ")
+        currentLowestRMSD.clear()
+        currentLowestRMSD.append({"temp": temperature, "rmsd": thisRMSD, "intensities": calculatedIntensities})
+    # the root mean square deviation cannot be lower that the one we already have, so let's
+    # discard it
+    else:
+        print("No change to lowest root mean square deviation we've found so far (", \
+              currentLowestRMSD[0]["rmsd"], \
+              " at temperature",
+              currentLowestRMSD[0]["temp"]), \
+            ")"
